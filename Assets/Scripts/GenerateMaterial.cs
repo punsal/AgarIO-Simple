1b121d97da2538baa4fc29fd5f8206f9f9499484
Mustafa Pamir Ünsal <p_unsal@msn.com>
2019-02-14 05:21:28 +0300

    Development Log
    Prefabs' templates created. Materials added in Resources folder. GenerateMaterials is working with tags. PlayerMovement requires a click on game screen to start movement and a second click to stop movement.

---
+++ Assets/Scripts/GenerateMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMaterial : MonoBehaviour
+{
+    private Material[] materials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Take materials from 
+        materials = Resources.LoadAll<Material>("Materials/" + transform.parent.tag);
+        Material randomMaterial = materials[Random.Range(0, materials.Length)];
+
+        GetComponent<Renderer>().material = randomMaterial; 
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private bool isClicked = false;
+
+    public bool IsClicked
+    {
+        get
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                switch (isClicked)
+                {
+                    case true:
+                        isClicked = false;
+                        break;
+                    case false:
+                        isClicked = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return isClicked;
+        }
+    }
+
+    private Vector3 mouseTarget;
+
+    public Vector3 MouseTarget
+    {
+        get
+        {
+            Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+
+            return target;
+        }
+    }
+
+
+
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsClicked)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                MouseTarget, speed * Time.deltaTime);
+        }
+    }
+}
