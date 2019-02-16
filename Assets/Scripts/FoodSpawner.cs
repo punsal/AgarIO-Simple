73cc34534bac77e0cef815761bbe089e66c28534
Mustafa Pamir Ünsal <pamir@happygamecompany.com>
2019-02-16 16:52:54 +0300

    Development Update
    An error about InvokeRepeating in FoodSpawner

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -2,26 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodSpawner : MonoBehaviour
-{
-    public Transform food;
+public class FoodSpawner : MonoBehaviour {
+    #region Food Attributes in Editor
+    public Transform foodObject;
     public float spawnSpeed = 2;
-
-    // Start is called before the first frame update
-    void Start()
+    public int foodPoolCapasity = 12;
+    #endregion
+    #region Singleton Attributes
+    private static FoodSpawner instance;
+    public static FoodSpawner Instance
     {
-        InvokeRepeating("Generate", 0, spawnSpeed);
+        get {
+            if(instance == null) {
+                instance = new FoodSpawner();
+            }
+            return instance;
+        }
+    }
+    #endregion
+    private Vector3 Target
+    {
+        get {
+            Vector3 target = Camera.main.ScreenToWorldPoint(
+                new Vector3(
+                    Random.Range(0, Camera.main.scaledPixelHeight * 2),
+                    Random.Range(0, Camera.main.scaledPixelWidth * 2),
+                    0));
+            target.z = 0;
+            return target;
+        }
+    }
+    private Queue<Transform> foodPool;
+
+    //private Constructor
+    private FoodSpawner() {
+        //Empty
     }
 
-    private void Generate()
-    {
-        Vector3 target = Camera.main.ScreenToWorldPoint(
-            new Vector3(
-                Random.Range(0, Camera.main.scaledPixelHeight * 2),
-                Random.Range(0, Camera.main.scaledPixelWidth * 2),
-                0));
-        target.z = 0;
+    // Start is called before the first frame update
+    private void Start() {
+        Instance.foodPool = new Queue<Transform>(12);
+        Instance.InvokeRepeating("Generate", 0, spawnSpeed);
+    }
 
-        Instantiate(food, target, Quaternion.identity);
+    public void Generate() {
+        if(Instance.foodPool.Count <= Instance.foodPoolCapasity) {
+            Transform food = Instantiate(
+                Instance.foodObject,
+                Instance.Target,
+                Quaternion.identity);
+            Instance.foodPool.Enqueue(food);
+        } else {
+            Transform food = Instance.foodPool.Dequeue();
+            food.position = Instance.Target;
+            Instance.foodPool.Enqueue(food);
+        }
     }
 }
